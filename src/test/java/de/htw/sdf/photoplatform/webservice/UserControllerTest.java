df4fb275e6ab586d59c4043cd4e508926ab0eccc
s.meister <s.meister>
2014-11-20 23:55:02 +0100

    PHOT-55: change ignore import package.

--- src/test/java/de/htw/sdf/photoplatform/webservice/UserControllerTest.java
+++ src/test/java/de/htw/sdf/photoplatform/webservice/UserControllerTest.java
@@ -4,10 +4,9 @@
 package de.htw.sdf.photoplatform.webservice;
 
 
-import jdk.nashorn.internal.ir.annotations.Ignore;
-
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
