d79c7f7cd5955762aa6ebf116785370b00154020
s.meister <s.meister>
2014-11-21 01:55:24 +0100

    PHOT-55: my first webservice test.

--- src/test/java/de/htw/sdf/photoplatform/webservice/UserControllerTest.java
+++ src/test/java/de/htw/sdf/photoplatform/webservice/UserControllerTest.java
@@ -4,15 +4,20 @@
 package de.htw.sdf.photoplatform.webservice;
 
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import de.htw.sdf.photoplatform.common.BaseTester;
+import de.htw.sdf.photoplatform.persistence.models.Role;
 
 /**
  * Tests for users services.
@@ -23,8 +28,7 @@ public class UserControllerTest extends BaseTester {
     private WebApplicationContext wac;
 
     @Before
-    public void setUp() throws Exception
-    {
+    public void setUp() throws Exception {
         mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
         insertTestData();
     }
@@ -35,14 +39,35 @@ public class UserControllerTest extends BaseTester {
     }
 
     @Test
-    @Ignore
-    public void testGetEnabledUsers(){
-        //TODO. coming soon :)!
+    public void testGetEnabledUsers() throws Exception {
+
+        mockMvc.perform(
+                get("/api/users/0/100")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .characterEncoding("UTF-8")).andExpect(status().isOk());
+        //TODO: Sergej Meister, fix me :)
+//        mockMvc.perform(
+//                get("/api/users/0/")
+//                        .accept(MediaType.APPLICATION_JSON)
+//                        .characterEncoding("UTF-8")).andExpect(status().isBadRequest());
     }
 
     @Test
-    @Ignore
-    public void testGetDisabledUsersByRole(){
-        //TODO. coming soon :)!
+    @Ignore("Don't work correct!")
+    public void testGetDisabledUsersByRole() throws Exception {
+        mockMvc.perform(
+                get("api/users/disabled/" + Role.PHOTOGRAPHER)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .characterEncoding("UTF-8")).andExpect(status().isOk());
+
+        mockMvc.perform(
+                get("api/users/disabled/" + Role.ADMIN)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .characterEncoding("UTF-8")).andExpect(status().isOk());
+
+        mockMvc.perform(
+                get("api/users/disabled/" + Role.CUSTOMER)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .characterEncoding("UTF-8")).andExpect(status().isOk());
     }
 }
