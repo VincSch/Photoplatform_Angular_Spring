86cfe23e92e80d70b3df4e790d10cec399b206d7
Patrick Marschallek <patrick.marschallek@gmail.com>
2014-12-03 23:08:22 +0100

    PHOT-19 - first thumbnail implementation

--- src/main/java/de/htw/sdf/photoplatform/webservice/controller/ImageController.java
+++ src/main/java/de/htw/sdf/photoplatform/webservice/controller/ImageController.java
@@ -7,12 +7,17 @@ import de.htw.sdf.photoplatform.persistence.model.Image;
 import de.htw.sdf.photoplatform.persistence.model.User;
 import de.htw.sdf.photoplatform.webservice.BaseAPIController;
 import org.h2.store.fs.FileUtils;
+import org.imgscalr.Scalr;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 
 /**
@@ -23,6 +28,15 @@ public class ImageController extends BaseAPIController {
     public static final String IMG_THUMBNAIL_URI = "/image/thumbnail.jpg";
     public static final String THUMBNAIL_REQ_URI = "/thumbnail";
     private static final String PREFIX = "uploads/";
+
+    private static final int THUMBNAIL_HEIGHT = 1024;
+    private static final int THUMBNAIL_WIDTH = 768;
+
+    private static final int MOBILE_THUMBNAIL_HEIGHT = 150;
+    private static final int MOBILE_THUMBNAIL_WIDTH = 150;
+
+    private static final int SMALL_THUMBNAIL_HEIGHT = 250;
+    private static final int SMALL_THUMBNAIL_WIDTH = 250;
 
     @Resource
     private ImageManager imageManager;
@@ -58,158 +72,47 @@ public class ImageController extends BaseAPIController {
                     type = type.split("/")[1];
                     try {
                         String hash = hashManager.hash(String.valueOf(id));
-                        String path =
-                            PREFIX + hash + "." + type;
-                        FileUtils.createFile(path);
+                        String path = PREFIX + hash;
+                        String originalPath = path + "." + type;
+                        String mobileThumbnailPath =
+                            path + "_thumbnail_mobile" + "." + type;
+                        String smallThumbnailPath =
+                            path + "_thumbnail_small" + "." + type;
+                        String thumbnailPath = path + "_thumbnail" + "." + type;
                         img.setEnabled(true);
                         img.setName(file.getOriginalFilename());
                         img.setPath(path);
                         img = imageManager.update(img);
-                    } catch (NoSuchAlgorithmException e) {
-                        e.printStackTrace();
+
+                        FileUtils.createFile(originalPath);
+                        createThumbnail(file, thumbnailPath, THUMBNAIL_WIDTH,
+                            THUMBNAIL_HEIGHT);
+                        createThumbnail(file, thumbnailPath,
+                            MOBILE_THUMBNAIL_WIDTH,
+                            MOBILE_THUMBNAIL_HEIGHT);
+                        createThumbnail(file, thumbnailPath,
+                            SMALL_THUMBNAIL_WIDTH,
+                            SMALL_THUMBNAIL_HEIGHT);
+
+                    } catch (NoSuchAlgorithmException | IOException e) {
+                        log.debug(e);
+                        return "false";
                     }
                 }
             }
         }
-        return "";
+        return "true";
     }
 
-    //    /**
-    //     * Accept an image upload via POST and notify a Reactor that the image needs to be thumbnailed. Asynchronously respond
-    //     * to the client when the thumbnailing has completed.
-    //     *
-    //     * @param channel   the channel on which to send an HTTP response
-    //     * @param thumbnail a reference to the shared thumbnail path
-    //     * @param reactor   the Reactor on which to publish events
-    //     * @return a consumer to handle HTTP requests
-    //     */
-    //    public static Consumer<FullHttpRequest> thumbnailImage(
-    //        NetChannel<FullHttpRequest, FullHttpResponse> channel,
-    //        AtomicReference<Path> thumbnail,
-    //        Reactor reactor) {
-    //        return req -> {
-    //            if (req.getMethod() != HttpMethod.POST) {
-    //                channel.send(badRequest(
-    //                    req.getMethod() + " not supported for this URI"));
-    //                return;
-    //            }
-    //
-    //            // write to a temp file
-    //            Path imgIn = null;
-    //            try {
-    //                imgIn = readUpload(req.content());
-    //            } catch (IOException e) {
-    //                throw new IllegalStateException(e.getMessage(), e);
-    //            }
-    //
-    //            // Asynchronously thumbnail the image to 250px on the long side
-    //            reactor.sendAndReceive("thumbnail", Event.wrap(imgIn), ev -> {
-    //                thumbnail.set(ev.getData());
-    //                channel.send(redirect());
-    //            });
-    //        };
-    //    }
-    //
-    //    /**
-    //     * Respond to GET requests and serve the thumbnailed image, a reference to which is kept in the given {@literal
-    //     * AtomicReference}.
-    //     *
-    //     * @param channel   the channel on which to send an HTTP response
-    //     * @param thumbnail a reference to the shared thumbnail path
-    //     * @return a consumer to handle HTTP requests
-    //     */
-    //    public static Consumer<FullHttpRequest> serveThumbnailImage(
-    //        NetChannel<FullHttpRequest, FullHttpResponse> channel,
-    //        AtomicReference<Path> thumbnail) {
-    //        return req -> {
-    //            if (req.getMethod() != HttpMethod.GET) {
-    //                channel.send(badRequest(
-    //                    req.getMethod() + " not supported for this URI"));
-    //            } else {
-    //                try {
-    //                    channel.send(serveImage(thumbnail.get()));
-    //                } catch (IOException e) {
-    //                    throw new IllegalStateException(e.getMessage(), e);
-    //                }
-    //            }
-    //        };
-    //    }
-    //
-    //    /**
-    //     * Respond to errors occurring on a Reactor by redirecting them to the client via an HTTP 500 error response.
-    //     *
-    //     * @param channel the channel on which to send an HTTP response
-    //     * @return a consumer to handle HTTP requests
-    //     */
-    //    public static Consumer<Throwable> errorHandler(
-    //        NetChannel<FullHttpRequest, FullHttpResponse> channel) {
-    //        return ev -> {
-    //            DefaultFullHttpResponse resp = new DefaultFullHttpResponse(
-    //                HttpVersion.HTTP_1_1,
-    //                HttpResponseStatus.INTERNAL_SERVER_ERROR);
-    //            resp.content().writeBytes(ev.getMessage().getBytes());
-    //            resp.headers().set(HttpHeaders.Names.CONTENT_TYPE, "text/plain");
-    //            resp.headers().set(HttpHeaders.Names.CONTENT_LENGTH,
-    //                resp.content().readableBytes());
-    //            channel.send(resp);
-    //        };
-    //    }
-    //
-    //    ////////////////////////// HELPER METHODS //////////////////////////
-    //  /*
-    //   * Read POST uploads and write them to a temp file, returning the Path to that file.
-    //   */
-    //    private static Path readUpload(ByteBuf content) throws IOException {
-    //        byte[] bytes = new byte[content.readableBytes()];
-    //        content.readBytes(bytes);
-    //        content.release();
-    //
-    //        // write to a temp file
-    //        Path imgIn = Files.createTempFile("upload", ".jpg");
-    //        Files.write(imgIn, bytes);
-    //
-    //        imgIn.toFile().deleteOnExit();
-    //
-    //        return imgIn;
-    //    }
-    //
-    //    /*
-    //     * Create an HTTP 400 bad request response.
-    //     */
-    //    public static FullHttpResponse badRequest(String msg) {
-    //        DefaultFullHttpResponse resp = new DefaultFullHttpResponse(HTTP_1_1,
-    //            BAD_REQUEST);
-    //        resp.content().writeBytes(msg.getBytes());
-    //        resp.headers().set(CONTENT_TYPE, "text/plain");
-    //        resp.headers().set(CONTENT_LENGTH, resp.content().readableBytes());
-    //        return resp;
-    //    }
-    //
-    //    /*
-    //     * Create an HTTP 301 redirect response.
-    //     */
-    //    public static FullHttpResponse redirect() {
-    //        DefaultFullHttpResponse resp = new DefaultFullHttpResponse(HTTP_1_1,
-    //            MOVED_PERMANENTLY);
-    //        resp.headers().set(CONTENT_LENGTH, 0);
-    //        resp.headers().set(LOCATION, IMG_THUMBNAIL_URI);
-    //        return resp;
-    //    }
-    //
-    //    /*
-    //     * Create an HTTP 200 response that contains the data of the thumbnailed image.
-    //     */
-    //    public static FullHttpResponse serveImage(Path path) throws IOException {
-    //        DefaultFullHttpResponse resp = new DefaultFullHttpResponse(HTTP_1_1,
-    //            OK);
-    //
-    //        RandomAccessFile f = new RandomAccessFile(path.toString(), "r");
-    //        resp.headers().set(CONTENT_TYPE, "image/jpeg");
-    //        resp.headers().set(CONTENT_LENGTH, f.length());
-    //
-    //        byte[] bytes = Files.readAllBytes(path);
-    //        resp.content().writeBytes(bytes);
-    //
-    //        return resp;
-    //    }
+    private void createThumbnail(MultipartFile file, String path, int width,
+        int height)
+        throws IOException {
+        BufferedImage image = ImageIO.read(file.getInputStream());
+        BufferedImage thumbnail =
+            Scalr.resize(image, Scalr.Method.SPEED, Scalr.Mode.FIT_TO_WIDTH,
+                width, height, Scalr.OP_ANTIALIAS);
+        File outputfile = new File(path);
+        ImageIO.write(thumbnail, "jpg", outputfile);
+    }
+
 }
