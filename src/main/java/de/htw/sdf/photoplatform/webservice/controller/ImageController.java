4816cc78b451f7a2bb577cc7ea28dcdfdf3f57a8
Patrick Marschallek <patrick.marschallek@gmail.com>
2014-12-04 21:50:59 +0100

    PHOT-19 - finished thumbnail implementation

--- src/main/java/de/htw/sdf/photoplatform/webservice/controller/ImageController.java
+++ src/main/java/de/htw/sdf/photoplatform/webservice/controller/ImageController.java
@@ -57,62 +57,94 @@ public class ImageController extends BaseAPIController {
 
         if (files.length != 0 &&
             userManager.isUserPhotographer(user)) {
-            if (!FileUtils.isDirectory(PREFIX)) {
-                if (FileUtils.exists(PREFIX)) {
-                    FileUtils.delete(PREFIX);
-                }
-                FileUtils.createDirectories(PREFIX);
-            }
+            this.createFilesystemStruture();
+
             for (MultipartFile file : files) {
-                Image img = new Image();
-                imageManager.create(img);
-                Long id = img.getId();
                 String type = file.getContentType();
                 if (type.startsWith("image")) {
                     type = type.split("/")[1];
                     try {
-                        String hash = hashManager.hash(String.valueOf(id));
-                        String path = PREFIX + hash;
-                        String originalPath = path + "." + type;
-                        String mobileThumbnailPath =
-                            path + "_thumbnail_mobile" + "." + type;
-                        String smallThumbnailPath =
-                            path + "_thumbnail_small" + "." + type;
-                        String thumbnailPath = path + "_thumbnail" + "." + type;
-                        img.setEnabled(true);
-                        img.setName(file.getOriginalFilename());
-                        img.setPath(path);
-                        img = imageManager.update(img);
-
-                        FileUtils.createFile(originalPath);
-                        createThumbnail(file, thumbnailPath, THUMBNAIL_WIDTH,
-                            THUMBNAIL_HEIGHT);
-                        createThumbnail(file, thumbnailPath,
-                            MOBILE_THUMBNAIL_WIDTH,
-                            MOBILE_THUMBNAIL_HEIGHT);
-                        createThumbnail(file, thumbnailPath,
-                            SMALL_THUMBNAIL_WIDTH,
-                            SMALL_THUMBNAIL_HEIGHT);
-
+                        String path = "";
+                        Image img = this.persistEntity(file, type);
+                        String originalPath = img.getPath();
+                        path = originalPath.split("/\\./")[0];
+                        BufferedImage image = ImageIO
+                            .read(file.getInputStream());
+                        this.saveImage(image, originalPath, type);
+                        this.processThumbnails(image, path,
+                            type);
                     } catch (NoSuchAlgorithmException | IOException e) {
                         log.debug(e);
                         return "false";
                     }
+                } else {
+                    log.warn("File isn't an image: unallowed type");
                 }
             }
         }
         return "true";
     }
 
-    private void createThumbnail(MultipartFile file, String path, int width,
+    private void processThumbnails(BufferedImage img, String path,
+        String type) throws IOException {
+
+        String mobileThumbnailPath =
+            path + "_thumbnail_mobile" + "." + type;
+        String smallThumbnailPath =
+            path + "_thumbnail_small" + "." + type;
+        String thumbnailPath = path + "_thumbnail" + "." + type;
+        createThumbnail(img, thumbnailPath,
+            type,
+            THUMBNAIL_WIDTH,
+            THUMBNAIL_HEIGHT);
+        createThumbnail(img, mobileThumbnailPath,
+            type,
+            MOBILE_THUMBNAIL_WIDTH,
+            MOBILE_THUMBNAIL_HEIGHT);
+        createThumbnail(img, smallThumbnailPath,
+            type,
+            SMALL_THUMBNAIL_WIDTH,
+            SMALL_THUMBNAIL_HEIGHT);
+    }
+
+    private void createFilesystemStruture() {
+        if (!FileUtils.isDirectory(PREFIX)) {
+            if (FileUtils.exists(PREFIX)) {
+                FileUtils.delete(PREFIX);
+            }
+            FileUtils.createDirectories(PREFIX);
+        }
+    }
+
+    private Image persistEntity(MultipartFile file, String type)
+        throws NoSuchAlgorithmException {
+        Image img = new Image();
+        imageManager.create(img);
+        String hash = hashManager.hash(String.valueOf(img.getId()));
+        String path = PREFIX + hash;
+        String originalPath = path + "." + type;
+
+        img.setEnabled(true);
+        img.setName(file.getOriginalFilename());
+        img.setPath(path);
+        return imageManager.update(img);
+    }
+
+    private boolean saveImage(BufferedImage img, String path, String type)
+        throws IOException {
+        File outputfile = new File(path);
+        return ImageIO.write(img, type, outputfile);
+    }
+
+    private void createThumbnail(BufferedImage image, String path, String type,
+        int width,
         int height)
         throws IOException {
-        BufferedImage image = ImageIO.read(file.getInputStream());
         BufferedImage thumbnail =
             Scalr.resize(image, Scalr.Method.SPEED, Scalr.Mode.FIT_TO_WIDTH,
-                width, height, Scalr.OP_ANTIALIAS);
-        File outputfile = new File(path);
-        ImageIO.write(thumbnail, "jpg", outputfile);
+                width, height);
+        if (!this.saveImage(thumbnail, path, type))
+            log.warn("Can't write Thumbnail. " + path);
     }
 
 }
