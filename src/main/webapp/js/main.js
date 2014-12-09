1a9449ee9027d90f97b33199215d44106f195310
Patrick Marschallek <patrick.marschallek@gmail.com>
2014-12-09 01:21:00 +0100

    PHOT-19 - layout changes and responsive enhancements

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,14 +1,34 @@
 /*
  Slidemenu
  */
-(function() {
+(function () {
     var body = document.body
         , menu_trigger = body.getElementsByClassName('menu-trigger')[0];
 
-    if ( typeof menu_trigger !== 'undefined' ) {
-        menu_trigger.addEventListener('click', function() {
-            body.className = ( body.className == 'menu-active' )? '' : 'menu-active';
+    if (typeof menu_trigger !== 'undefined') {
+        menu_trigger.addEventListener('click', function () {
+            body.className = ( body.className == 'menu-active' ) ? '' : 'menu-active';
         });
     }
 
 }).call(this);
+
+function fadeOutMenu() {
+    $('body').removeClass('menu-active');
+    return false;
+}
+
+function removePreviewImage(element) {
+    $('input[type="file"]')[0].files = null;
+    //var files = $('input[type="file"]')[0].files;
+    //var img = $(element).prev();
+    //for(var i=0;files.length>i;i++){
+    //    if (files[i].name === img.attr('alt')) {
+    //        files.splice(i, 1);
+    //    }
+    //}
+    if (true || files.length)
+        $(".upload").prop('disabled', true);
+    $('#preview').html("");
+    $('#preview').hide();
+}
