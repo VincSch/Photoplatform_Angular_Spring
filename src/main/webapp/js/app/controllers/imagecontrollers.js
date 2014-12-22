b257ae52e2db46f80976ec687cad02e8ccb1af15
VincSch <vs@stroodel.com>
2014-12-22 17:54:15 +0100

    Added ajax loader for image upload

--- src/main/webapp/js/app/controllers/imagecontrollers.js
+++ src/main/webapp/js/app/controllers/imagecontrollers.js
@@ -1,6 +1,6 @@
 photoplatformControllers.controller('ImageCtrl',
-    ['$scope', '$rootScope', '$location', '$http', '$cookieStore', '$route', 'UserService', 'ImageService',
-        function ($scope, $rootScope, $location, $http, $cookieStore, $route, UserService, ImageService) {
+    ['$scope', '$rootScope', '$location', '$http', '$cookieStore', '$route', 'UserService', 'ImageService','$modal',
+        function ($scope, $rootScope, $location, $http, $cookieStore, $route, UserService, ImageService, $modal) {
             var user = $cookieStore.get('user');
             //if user is not logged in or logged in redirect to login page
             if (undefined == user || !$rootScope.isLoggedIn()) {
@@ -16,8 +16,14 @@ photoplatformControllers.controller('ImageCtrl',
 
             $scope.uploadImage = function () {
                 var param = $scope.files;
+                $scope.modalInstance = $modal.open({
+                    templateUrl: '/views/partials/profile/photographer/image/uploadIndicator.html',
+                    windowClass: 'upload-dialog'
+                });
+
                 ImageService.upload(param, $rootScope.user)
                     .success(function (message) {
+                        $scope.modalInstance.close();
                         $('#preview').hide();
                         $('#preview').html('');
                         $('#upload').prop('disabled', true);
@@ -69,4 +75,5 @@ photoplatformControllers.controller('ImageCtrl',
                 })
             };
 
-        }]);
+        }])
+;
