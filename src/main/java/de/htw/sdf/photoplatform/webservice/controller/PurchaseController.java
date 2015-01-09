b617ca25bc5fb28c3a438a31c09e7f0cbdbc96c9
s.meister <s.meister>
2015-01-09 17:47:36 +0100

    PHOT-33: webservice to get images in shopping cart and to get images which user has purchased.

--- src/main/java/de/htw/sdf/photoplatform/webservice/controller/PurchaseController.java
+++ src/main/java/de/htw/sdf/photoplatform/webservice/controller/PurchaseController.java
@@ -7,10 +7,14 @@ import de.htw.sdf.photoplatform.exception.BadRequestException;
 import de.htw.sdf.photoplatform.exception.common.AbstractBaseException;
 import de.htw.sdf.photoplatform.exception.common.ManagerException;
 import de.htw.sdf.photoplatform.manager.PurchaseManager;
+import de.htw.sdf.photoplatform.persistence.model.Image;
 import de.htw.sdf.photoplatform.persistence.model.PurchaseItem;
 import de.htw.sdf.photoplatform.persistence.model.User;
 import de.htw.sdf.photoplatform.webservice.BaseAPIController;
 import de.htw.sdf.photoplatform.webservice.Endpoints;
+import de.htw.sdf.photoplatform.webservice.dto.ImageData;
+import de.htw.sdf.photoplatform.webservice.dto.PurchaseData;
+import de.htw.sdf.photoplatform.webservice.util.ResourceUtility;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -83,5 +87,36 @@ public class PurchaseController extends BaseAPIController {
 
             throw new BadRequestException(exceptionMsg);
         }
+    }
+
+    /**
+     * Returns PurchaseData, that represent a shopping cart.
+     * <p/>
+     * PurchaseData include images, total price of all images
+     * and count of all images in shopping cart.
+     *
+     * @return PurchaseData.
+     */
+    @RequestMapping(value = Endpoints.PURCHASES, method = RequestMethod.GET)
+    @ResponseBody
+    public PurchaseData getShoppingCartData() {
+
+        User user = getAuthenticatedUser();
+        List<PurchaseItem> itemInShoppingCart = purchaseManager.getItemsInShoppingCart(user);
+        Double totalPrice = purchaseManager.calculatePrice(itemInShoppingCart);
+        return ResourceUtility.convertToPurchaseData(itemInShoppingCart, totalPrice);
+    }
+
+    /**
+     * Returns all images, which user has purchased.
+     *
+     * @return list of images belong to the user.
+     */
+    @RequestMapping(value = Endpoints.USERS_IMAGES, method = RequestMethod.GET)
+    @ResponseBody
+    public List<ImageData> getUserImages() {
+        User user = getAuthenticatedUser();
+        List<Image> userImages = purchaseManager.getUserImages(user);
+        return ResourceUtility.convertListToImageData(userImages);
     }
 }
