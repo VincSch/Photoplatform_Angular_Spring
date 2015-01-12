d5dc18d8e1db19647f617a4dbf6e1ac16a96dcc9
VincSch <vs@stroodel.com>
2015-01-12 20:39:55 +0100

    first show of ui code

---
+++ src/main/webapp/js/app/controllers/shoppinglistcontrollers.js
@@ -0,0 +1,7 @@
+/**
+ * Image search controller.
+ */
+photoplatformControllers.controller('ShoppingListCtrl', ['$scope', '$rootScope', 'ShoppingListService', '$location',
+    function ($scope, $rootScope, ShoppingListService, $location) {
+
+    }]);
