0420e3a79ca1c86d734bc0ae814894d90b1b1e98
VincSch <vs@stroodel.com>
2015-01-15 00:21:05 +0100

    Merge branch 'PHOT-34' finished implementation

--- src/main/webapp/js/app/controllers/shoppinglistcontrollers.js
+++ src/main/webapp/js/app/controllers/shoppinglistcontrollers.js
@@ -4,4 +4,32 @@
 photoplatformControllers.controller('ShoppingListCtrl', ['$scope', '$rootScope', 'ShoppingListService', '$location',
     function ($scope, $rootScope, ShoppingListService, $location) {
 
+        $scope.encodedToken = encode($rootScope.user.secToken);
+
+        function encode(token){
+            var replacedAt = token.replace("@", "(at)");
+            var replacedDP = replacedAt.replace(".", "(dot)");
+            var encodedToken = replacedDP.replace(":", "(colon)");
+            return encodedToken
+        }
+
+        ShoppingListService.getPurchasedImages().success(function (images) {
+            $scope.images = images;
+        }).error(function (error) {
+            console.log(error);
+            $rootScope.error(error);
+        });
+
+        /**
+         * Get raw image
+         * @param imageId
+         */
+        $scope.downloadImage = function (imageId) {
+            ShoppingListService.getRawImage(imageId).success(function (rawImage) {
+
+            }).error(function (data) {
+                $scope.errors = data.errors;
+            });
+        };
+
     }]);
--- src/main/webapp/js/app/services/shoppinglist.js
+++ src/main/webapp/js/app/services/shoppinglist.js
@@ -2,11 +2,32 @@
  * AuthService.
  * Login, register and passwordLost and passwordReset.
  */
-angular.module('photoplatform').factory('ShoppingListService', ['$http',
-    function ($http) {
+angular.module('photoplatform').factory('ShoppingListService', ['$http', '$rootScope',
+    function ($http, $rootScope) {
 
-        var urlBase = '/api/purchase';
+        var urlBase = '/api/';
 
         return {
+            /**
+             * Get image as bytes
+             *
+             * @param image id
+             * @returns {HttpPromise}
+             */
+            getRawImage: function (imageId) {
+                $http.defaults.headers.common[xAuthTokenHeaderName] = $rootScope.user.secToken;
+                return $http.get(urlBase + 'image/' + imageId);
+            },
+
+            /**
+             * Get collection
+             *
+             * @param name
+             * @param description
+             * @returns {HttpPromise}
+             */
+            getPurchasedImages: function () {
+                return $http.get(urlBase + 'users/images');
+            }
         }
     }]);
