8863f97ef7cd5ea8775698925916cb837f3648c9
VincSch <vs@stroodel.com>
2015-01-18 12:57:47 +0100

    added user login check for shopping list

--- src/main/webapp/js/app/controllers/shoppinglistcontrollers.js
+++ src/main/webapp/js/app/controllers/shoppinglistcontrollers.js
@@ -4,6 +4,13 @@
 photoplatformControllers.controller('ShoppingListCtrl', ['$scope', '$rootScope', 'ShoppingListService', '$location', 'md5',
     function ($scope, $rootScope, ShoppingListService, $location, md5) {
 
+        var user = $cookieStore.get('user');
+        //if user is not logged in or logged in redirect to login page
+        if (user == undefined || !$rootScope.isLoggedIn()) {
+            $location.path("/login");
+            return;
+        }
+        
         $scope.encodedToken = md5.createHash($rootScope.user.secToken);
         $scope.userId = $rootScope.user.id;
 
